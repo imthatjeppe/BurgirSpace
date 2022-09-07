5acd4e0f377a63d8ad3e4369b1f858eddb08f1d3
Jesper Johansson <33457842+imthatjeppe@users.noreply.github.com>
2022-09-07 15:33:25 +0200

    Work In progress

--- Assets/Scripts/HandPhysics.cs
+++ Assets/Scripts/HandPhysics.cs
@@ -5,18 +5,34 @@ using UnityEngine;
 public class HandPhysics : MonoBehaviour
 {
     public Transform target;
-    private Rigidbody rb;
+    //public Renderer noPhysicalHand;
+    //public float showNonPhysicalDistance = 0.05f;
 
+    private Rigidbody rb;
     private Collider[] handColliders;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         handColliders = GetComponentsInChildren<Collider>();
+        Physics.IgnoreLayerCollision(6, 7, true);
+        Physics.IgnoreLayerCollision(6, 8, true);
+    }
+
+    private void Update()
+    {
+        //float distance = Vector3.Distance(transform.position, target.position);
+
+        //if (distance > showNonPhysicalDistance)
+        //{
+        //    noPhysicalHand.enabled = true;
+        //}
+        //else
+        //    noPhysicalHand.enabled = false;
     }
 
     public void EnableHandColliders()
     {
-        foreach(var item in handColliders)
+        foreach (var item in handColliders)
         {
             item.enabled = true;
         }
@@ -29,7 +45,10 @@ public class HandPhysics : MonoBehaviour
         }
     }
 
-
+    public void EnableHandCollidersDelay(float delay)
+    {
+        Invoke("EnableHandColliders", delay);
+    }
     private void FixedUpdate()
     {
         //Position
