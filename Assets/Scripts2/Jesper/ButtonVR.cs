c0c6a0687ee5036b819f20cfca9c788ff8882d78
Jesper Johansson <33457842+imthatjeppe@users.noreply.github.com>
2022-09-12 09:57:22 +0200

    fixes

---
+++ Assets/Scripts2/Jesper/ButtonVR.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ButtonVR : MonoBehaviour
+{
+    public GameObject button;
+    public UnityEvent onPress;
+    public UnityEvent onRelease;
+
+    GameObject presser;
+    bool isPressed; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        isPressed = false;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (!isPressed)
+        {
+            button.transform.localPosition = new Vector3(0, 0.03f, 0);
+            presser = other.gameObject;
+            onPress.Invoke();
+            isPressed = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other == presser)
+        {
+            button.transform.localPosition = new Vector3(0, 0.015f, 0);
+            onRelease.Invoke();
+            isPressed = false;
+        }
+    }
+
+    public void SpawnSphere()
+    {
+        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+        sphere.transform.localPosition = new Vector3(0, 1, 2);
+        sphere.AddComponent<Rigidbody>();
+    }
+}
