91d1407ab2dc1cb9865ccee218624a00b4d800ad
Jesper Johansson <33457842+imthatjeppe@users.noreply.github.com>
2022-09-23 12:37:32 +0200

    yes

--- Assets/Scripts/Jesper/Food/FoodBlender.cs
+++ Assets/Scripts/Jesper/Food/FoodBlender.cs
@@ -6,10 +6,8 @@ public class FoodBlender : MonoBehaviour
 {
     public GameObject spawnTube;
     public GameObject addFoodTube;
-
     public List<GameObject> foodItems;
 
-    private FoodID foodID;
     private Vector3 offset = new Vector3(0.15f, 0, 0);
 
     private void OnTriggerEnter(Collider other)
@@ -17,8 +15,6 @@ public class FoodBlender : MonoBehaviour
         if (other.gameObject.CompareTag("Food"))
         {
             var foodID = other.GetComponent<FoodID>();
-            //foodID = FindObjectOfType<FoodID>();
-
             Destroy(other.gameObject);
 
             for (int i = 0; i < foodID.amountItems; i++)
