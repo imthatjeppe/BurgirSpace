77c42f850becf546071686e8ab43d2dce05b394b
Jesper Johansson <33457842+imthatjeppe@users.noreply.github.com>
2022-09-27 14:42:11 +0200

    Cringe

--- Assets/Scripts/Niklas/FoodSpawner.cs
+++ Assets/Scripts/Niklas/FoodSpawner.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class FoodSpawner : MonoBehaviour
 {
-    [SerializeField] GameObject spawnPointTopBun, spawnPointBottomBun, spawnPointPatty;
+    [SerializeField] GameObject spawnPointTopBun, spawnPointBottomBun, spawnPointPatty, spawnPointPlate;
 
     void Update()
     {
@@ -18,5 +18,10 @@ public class FoodSpawner : MonoBehaviour
         ObjectPooler.instance.SpawnFromPool("TopBun", spawnPointTopBun.transform.position, Quaternion.Euler(-90, 0, 0));
         ObjectPooler.instance.SpawnFromPool("Patty", spawnPointPatty.transform.position, Quaternion.Euler(-90, 0, 0));
         ObjectPooler.instance.SpawnFromPool("BottomBun", spawnPointBottomBun.transform.position, Quaternion.Euler(-90, 0, 0));
+    }
+
+    public void SpawnPlate()
+    {
+        ObjectPooler.instance.SpawnFromPool("Plate", spawnPointTopBun.transform.position, spawnPointTopBun.transform.rotation);
     }
 }
