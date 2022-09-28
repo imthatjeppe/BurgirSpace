df8d555990b2151d0b81e3dd4ea5bf8e5717ef95
Marcus Rundén <marcus.runden@hotmail.com>
2022-09-28 13:38:16 +0200

    Fixed typos
    Fixed typos

--- Assets/Scripts/Jesper/Destruction.cs
+++ Assets/Scripts/Jesper/Destruction.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class Destruction : MonoBehaviour
 {
-    GameObject destructableObject;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("NonInteractable"))
@@ -19,7 +18,7 @@ public class Destruction : MonoBehaviour
                     Destroy(gameObject, 3f);
                 }
             }
-            AudioManager.instance.PlayOnceLocal("Plate breaking", destructableObject);
+            AudioManager.instance.PlayOnceLocal("Plate breaking", gameObject);
         }
     }
 }
--- Assets/Scripts/Niklas/FoodSpawner.cs
+++ Assets/Scripts/Niklas/FoodSpawner.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 
 public class FoodSpawner : MonoBehaviour
 {
-    GameObject spawnPipe;
     [SerializeField] GameObject spawnPointTopBun, spawnPointBottomBun, spawnPointPatty, spawnPointPlate;
 
     #region Singleton
@@ -27,7 +26,7 @@ public class FoodSpawner : MonoBehaviour
         ObjectPooler.instance.SpawnFromPool("TopBun", spawnPointTopBun.transform.position, Quaternion.Euler(-90, 0, 0));
         ObjectPooler.instance.SpawnFromPool("Patty", spawnPointPatty.transform.position, Quaternion.Euler(-90, 0, 0));
         ObjectPooler.instance.SpawnFromPool("BottomBun", spawnPointBottomBun.transform.position, Quaternion.Euler(-90, 0, 0));
-        AudioManager.instance.PlayOnceLocal("Food Spawn", spawnPipe);
+        AudioManager.instance.PlayOnceLocal("Food spawn", gameObject);
     }
 
     public void SpawnPlate()
