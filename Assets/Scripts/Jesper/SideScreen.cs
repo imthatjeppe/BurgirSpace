95febf9d51e6699310e126dd96319185fb6f628b
Niklsa <niklio15csgo@hotmail.com>
2022-09-28 13:08:48 +0200

    Arm display added

--- Assets/Scripts/Jesper/SideScreen.cs
+++ Assets/Scripts/Jesper/SideScreen.cs
@@ -1,19 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class SideScreen : MonoBehaviour
 {
     public Transform target;
     public Vector3 offset;
+    public TMP_Text order;
     // Start is called before the first frame update
 
-    private void FixedUpdate()
+    void Update()
     {
         transform.position = target.position + Vector3.up * offset.y
             + Vector3.ProjectOnPlane(target.right, Vector3.up).normalized * offset.x
             + Vector3.ProjectOnPlane(target.forward, Vector3.up).normalized * offset.z;
+    }
 
-        transform.eulerAngles = new Vector3(0, target.eulerAngles.y, 0);
+    public void SetOrder()
+    {
+        for(int i = 0; i < OrderManager.instance.orderItems.Count; i++)
+        {
+            order.text = order.text + "\n" + OrderManager.instance.orderItems[i].IngredientName;
+        }
     }
 }
