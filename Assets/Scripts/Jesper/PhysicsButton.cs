3fe24a679e976fba8643f6511efb2b58d1682255
Marcus Rundén <marcus.runden@hotmail.com>
2022-09-28 16:27:11 +0200

    Added more sounds

--- Assets/Scripts/Jesper/PhysicsButton.cs
+++ Assets/Scripts/Jesper/PhysicsButton.cs
@@ -43,6 +43,7 @@ public class PhysicsButton : MonoBehaviour
     {
         isPressed = true;
         onPressed.Invoke();
+        AudioManager.instance.PlayOnceLocal("Button", gameObject);
     }
 
     private void Released()
