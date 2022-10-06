3ca011c94e06b2ee83a2e79a570bf56020f3dcd8
Jesper Johansson <33457842+imthatjeppe@users.noreply.github.com>
2022-10-06 14:16:07 +0200

    Fixed

--- Assets/Scripts/Jesper/AISpawnPosition.cs
+++ Assets/Scripts/Jesper/AISpawnPosition.cs
@@ -6,22 +6,30 @@ public class AISpawnPosition : MonoBehaviour
 {
     public GameObject[] robots;
     public List<GameObject> aiSpawnPosition = new List<GameObject>();
-    public int positions;
+    int positions;
 
     int randomIndex;
     int positionsUsed;
+    int amountOfRobots = 0;
     void Start()
     {
         randomIndex = Random.Range(0, robots.Length);
+        positionsUsed = Random.Range(1, positions);
+        positions = aiSpawnPosition.Count;
+
+        InvokeRepeating("SpawnRobots", 0, 3f);
+
+    }
+
+    void SpawnRobots()
+    {
+        if (amountOfRobots >= positionsUsed) {CancelInvoke("SpawnRobots"); return; }
+
+        GameObject Robot = Instantiate(robots[randomIndex], aiSpawnPosition[positionsUsed].transform.position, aiSpawnPosition[positionsUsed].transform.rotation);
+        Robot.transform.GetChild(0).GetComponent<Animator>().SetTrigger("Eating");
+        randomIndex = Random.Range(0, robots.Length);
         positionsUsed = Random.Range(0, positions);
 
-        for (int i = 0; i < positionsUsed; i++)
-        {
-            GameObject Robot = Instantiate(robots[randomIndex], aiSpawnPosition[positionsUsed].transform.position, aiSpawnPosition[positionsUsed].transform.rotation);
-            Robot.transform.GetChild(0).GetComponent<Animator>().SetTrigger("Eating");
-            randomIndex = Random.Range(0, robots.Length);
-            positionsUsed = Random.Range(0, positions);
-        }
-
+        amountOfRobots++;
     }
 }
