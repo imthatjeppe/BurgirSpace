610d6c46242ec4501b6292728f9dee0a8cfda781
Marcus Rundén <marcus.runden@hotmail.com>
2022-10-06 14:28:31 +0200

    Added functionality and text to the tutorials
    Added functionality and text to the tutorials

---
+++ Assets/Scripts/Marcus/Tutorial/EndTutorial.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndTutorial : MonoBehaviour
+{
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene("Main Level");
+    }
+}
