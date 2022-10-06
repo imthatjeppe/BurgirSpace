01a47560d3e42b16f1fbc0803d50b9e3c0485344
Jesper Johansson <33457842+imthatjeppe@users.noreply.github.com>
2022-10-06 15:51:41 +0200

    Yes

--- Assets/Scripts/Jesper/Food/FoodBlender.cs
+++ Assets/Scripts/Jesper/Food/FoodBlender.cs
@@ -7,6 +7,7 @@ public class FoodBlender : MonoBehaviour
     public GameObject spawnTube;
     public GameObject addFoodTube;
     public List<GameObject> foodItems;
+    FoodID foodID;
 
     public Vector3 offset = new Vector3(0, 0, 0);
 
@@ -14,15 +15,21 @@ public class FoodBlender : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Food"))
         {
-            var foodID = other.GetComponent<FoodID>();
+            foodID = other.GetComponent<FoodID>();
             Destroy(other.gameObject);
             AudioManager.instance.PlayOnceLocal("Food mixer", gameObject);
+            transform.parent.gameObject.GetComponent<Animator>().SetTrigger("Shake");
 
-            for (int i = 0; i < foodID.amountItems; i++)
-            {
-                GameObject obj = Instantiate(foodItems[foodID.foodID], spawnTube.transform.position + offset, Quaternion.identity);
-                obj.GetComponent<Rigidbody>().AddForce(Vector3.forward, ForceMode.Impulse);
-            }
+            Invoke("MixVegetable", 1f);
+        }
+    }
+
+    void MixVegetable()
+    {
+        for (int i = 0; i < foodID.amountItems; i++)
+        {
+            GameObject obj = Instantiate(foodItems[foodID.foodID], spawnTube.transform.position, Quaternion.identity);
+            obj.GetComponent<Rigidbody>().AddForce(offset, ForceMode.Impulse);
         }
     }
 }
